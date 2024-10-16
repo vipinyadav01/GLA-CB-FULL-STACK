a297b493cc418d1730a79aba7f02bbf6e9d45464
vipin yadav <vipinyadav9m@gmail.com>
2024-10-16 10:01:06 +0530

    by using clouser property

--- function/function.js
+++ function/function.js
@@ -11,17 +11,32 @@
 // let returnedfunction = fun1();
 // consol.log(returnedfunction(a, b));
 
+//-----------------direct call -----------------//
+// function fun1() {
+//   let a = 10;
+//   let b = 20;
+
+//   function fun2(a, b) {
+//     return a + b;
+//   }
+//   return funtion ko krege uske baad fun2 ko krege
+//   return function () {
+//     return fun2(a, b);
+//   };
+// }
+
+// let returnedFunction = fun1();
+// console.log(returnedFunction());
+
+//-------------using closure-----------//
 function fun1() {
   let a = 10;
   let b = 20;
 
-  function fun2(a, b) {
+  function fun2() {
     return a + b;
   }
-  //reurn funtion ko krege uske baad fun2 ko krege
-  return function () {
-    return fun2(a, b);
-  };
+  return fun2;
 }
 
 let returnedFunction = fun1();
