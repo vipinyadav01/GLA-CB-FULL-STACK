cccad4b64fdde50511553d8183f47e682e3183ca
vipin yadav <vipinyadav9m@gmail.com>
2024-11-06 09:56:46 +0530

    DOM-06-11-2024

---
+++ D-O-M/DOM.js
@@ -0,0 +1,124 @@
+// let button = document.getElementById("btn");
+// button.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+
+// document.getElementById("btn").addEventListener("click", () => {
+//   console.log("clicked");
+// });
+
+// document.querySelector("#btn").addEventListener("click", () => {
+//   console.log("clicked");
+// });
+//
+//
+//
+//
+//innerHTML
+// let element = document.getElementById("btn");
+// element.innerHTML = "Hello World";
+//
+//
+//innerText
+// let element = document.getElementById("btn");
+// element.innerText = "Hello World";
+//
+//
+//
+//
+//textContent
+// let element = document.getElementById("btn");
+// element.textContent = "Hello World";
+//
+//
+//
+//
+// classList
+// let element = document.getElementById("btn");
+// element.classList.add("btn");
+// element.classList.remove("btn");
+// element.classList.toggle("btn");
+// element.classList.contains("btn");
+// element.classList.replace("btn", "btn2");
+//
+//
+//
+//
+// style
+// let element = document.getElementById("btn");
+// element.style.color = "red";
+// element.style.backgroundColor = "blue";
+// element.style.width = "100px";
+// element.style.height = "100px";
+//
+//
+//
+//
+//
+// getAttribute
+// let element = document.getElementById("btn");
+// console.log(element.getAttribute("id"));
+// console.log(element.getAttribute("class"));
+//
+//
+//
+//
+//
+// setAttribute
+// let element = document.getElementById("btn");
+// element.setAttribute("id", "btn2");
+// element.setAttribute("class", "btn2");
+//
+//
+//
+//
+// removeAttribute
+// let element = document.getElementById("btn");
+// element.removeAttribute("id");
+// element.removeAttribute("class");
+//
+//
+//
+//
+// addEventListener
+// let element = document.getElementById("btn");
+// element.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+//
+//
+//
+//
+// removeEventListener
+// let element = document.getElementById("btn");
+// element.removeEventListener("click", () => {
+//   console.log("clicked");
+// });
+//
+//
+//
+//
+// submit
+// let element = document.getElementById("btn");
+// element.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   console.log("submitted");
+// });
+//
+//
+//
+//
+// focus
+// let element = document.getElementById("btn");
+// element.addEventListener("focus", () => {
+//   console.log("focused");
+// });
+//
+//
+//
+//
+// blur
+// let element = document.getElementById("btn");
+// element.addEventListener("blur", () => {
+//   console.log("blurred");
+// });
